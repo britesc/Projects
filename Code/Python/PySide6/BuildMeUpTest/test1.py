6d52e571fd231607bfff48fd82b9de7057dc2a5d
Julian Bourne <julian@britesc.com>
2023-01-18 23:10:20 +0100

    Finished FindApps Class

--- Code/Python/PySide6/BuildMeUpTest/test1.py
+++ Code/Python/PySide6/BuildMeUpTest/test1.py
@@ -10,51 +10,48 @@ import re
 ## This is a list of needed applications
 #  It contains dictionaries
 
-applicationsNeededOld = [
-    "Python3",
-    "CMake",
-    "Dot",
-    "Dia",
-    "Doxygen",
-    "Ninja",
-    "Git"    
-    ]
-
 applicationsNeeded = [
     {
         "AppName": "Python3",
         "AppVerGet": "--version",
-        "AppRegex": "None"
+        "RegexStart": 7,
+        "RegexEnd": 13
     },
     {
         "AppName": "CMake",
         "AppVerGet": "--version",
-        "AppRegex": "None"
+        "RegexStart": 14,
+        "RegexEnd": 20
     },
     {
         "AppName": "Dot",
         "AppVerGet": "-V",
-        "AppRegex": "None"
+        "RegexStart": 22,
+        "RegexEnd": 29
     },
     {
         "AppName": "Dia",
         "AppVerGet": "-v",
-        "AppRegex": "None"
+        "RegexStart": 12,
+        "RegexEnd": 18
     },
     {
         "AppName": "Doxygen",
         "AppVerGet": "--version",
-        "AppRegex": "None"
+        "RegexStart": 0,
+        "RegexEnd": 5
     },
     {
         "AppName": "Ninja",
         "AppVerGet": "--version",
-        "AppRegex": "None"
+        "RegexStart": 0,
+        "RegexEnd": 6
     },
     {
         "AppName": "Git",
         "AppVerGet": "-v",
-        "AppRegex": "None"
+        "RegexStart": 12,
+        "RegexEnd": 20
     }    
 ]
 
@@ -62,7 +59,6 @@ applicationsFound = []
 
 
 class FindApps:
-    print("Checking for Files Required for the System")
     
     def __init__(self):
         pass
@@ -72,56 +68,38 @@ class FindApps:
     
 
     def search():
-        matches=[]
         # Read the list of programs
 
         for dict_item in applicationsNeeded:
             vAppName = dict_item['AppName']
             vAppVerGet = dict_item['AppVerGet']
-            vAppRegex = dict_item['AppRegex']            
-  
-            #print(f'vAppName = {vAppName}')
-            #print(f'vAppVerGet = {vAppVerGet}')
-            #print(f'vAppRegex = {vAppRegex}')
+            vRegexStart = dict_item['RegexStart']            
+            vRegexEnd = dict_item['RegexEnd']
                                    
             for root, dirnames, filenames in os.walk('/usr/bin'):
                 for filename in fnmatch.filter(filenames, vAppName.lower()):
-                    flute = os.path.join(root, filename)
-                    request = flute + " " + vAppVerGet
-                    process = subprocess.run(request, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+                    vPathJoin = os.path.join(root, filename)
+                    vRequest = vPathJoin + " " + vAppVerGet
+                    process = subprocess.run(vRequest, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
                     
                     responseVersion = process.stdout
                     if not responseVersion:
                         responseVersion = process.stderr
                     responseVersion = responseVersion.rstrip('\n')
-                    str = responseVersion
-                    try:
-                        found = re.search('git version (.+?) ', str).group(1)
-                        print(found)
-                    except AttributeError:
-                        pass
+                    found = responseVersion[vRegexStart:vRegexEnd]
                     
                     applicationsFound.append(
                         {
                             'app': vAppName,
-                            'location':  flute,
-                            'version': responseVersion 
+                            'location':  vPathJoin,
+                            'version': found 
                         }
                     )                                
-        print(f'Applications Found = {applicationsFound}')
         
 
-
-"""git version 2.37.2
-str = "123AAATorontoZZZ123"
-try:
-    found = re.search('git version (.+?) ', str).group(1)
-    print(found)
-except AttributeError:
-    pass
-"""
 def main():
     FindApps.search()
+    print(f'{applicationsFound}')
     
 
 if __name__ == '__main__':
