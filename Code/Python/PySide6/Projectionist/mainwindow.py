8e43ef49e97fd29a08232e882ec18e34fcf11874
Julian Bourne <julian@britesc.com>
2023-01-26 18:54:50 +0100

    Getting Closer

--- Code/Python/PySide6/Projectionist/mainwindow.py
+++ Code/Python/PySide6/Projectionist/mainwindow.py
@@ -10,4 +10,28 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.app = app
         
 
-        
+        self.actionExit.triggered.connect(self.exit)
+        self.actionSettings.triggered.connect(self.settings)
+        self.actionLight.triggered.connect(self.light)
+        self.actionDark.triggered.connect(self.dark)
+        self.actionAuto.triggered.connect(self.auto)
+        self.actionTheme.triggered.connect(self.theme)
+        self.actionAbout.triggered.connect(self.about)
+        self.actionAboutQt.triggered.connect(self.aboutQt)
+
+    def exit(self):
+        self.app.quit()
+    def settings(self):
+        pass
+    def light(self):
+        pass
+    def dark(self):
+        pass
+    def auto(self):
+        pass
+    def theme(self):
+        pass
+    def about(self):
+        QMessageBox.information(self,"Going pro!","QMainWindow,Qt Designer and Resources : Going pro!")
+    def aboutQt(self):
+        QApplication.aboutQt()    
