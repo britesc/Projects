30f58028c248768e5f54a4bf0255c0f947f13c74
Julian Bourne <julian@britesc.com>
2023-01-29 12:23:29 +0100

    Flake8 Test

--- Code/Python/PySide6/Projectionist/mainwindow.py
+++ Code/Python/PySide6/Projectionist/mainwindow.py
@@ -1,15 +1,16 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QMainWindow,QMessageBox
-from PyQt6.QtCore import QSettings, QCoreApplication
+#from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt6.QtCore import QSettings
 
 from ui_mainwindow import Ui_MainWindow
 
+
 class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self,app):
+    def __init__(self, app):
         super().__init__()
         self.setupUi(self)
         self.app = app
-        
+
         self.setWindowTitle("Projectionist Configuration")
 
         self.actionExit.triggered.connect(self.exit)
@@ -23,43 +24,49 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.pushButtonProjectFolderLocate.clicked.connect(self.ProjectFolderLocate)
         self.pushButtonProjectFolderSave.clicked.connect(self.dummy_function)
         self.pushButtonProjectFolderCancel.clicked.connect(self.dummy_function)
-        
-    def dummy_function(self):
-        print(f"Dummy Function Called")
-    def exit(self):
+
+    def dummy_function(self) -> None:
+        print("Dummy Function Called")
+
+    def exit(self) -> None:
         self.app.quit()
-    def settings(self):
+
+    def settings(self) -> None:
         pass
-    def light(self):
+
+    def light(self) -> None:
         pass
-    def dark(self):
+
+    def dark(self) -> None:
         pass
-    def auto(self):
+
+    def auto(self) -> None:
         pass
-    def theme(self):
+
+    def theme(self) -> None:
         pass
-    def about(self):
-        QMessageBox.information(self,"Going pro!","QMainWindow,Qt Designer and Resources : Going pro!")
-    def aboutQt(self):
-        QApplication.aboutQt()    
-    def ProjectFolderLocate(self):
+
+    def about(self) -> None:
+        QMessageBox.information(self, "Rubbish!", " Rubbish!")
+  
+    def aboutQt(self) -> None:
+        QApplication.aboutQt()
+    
+    def ProjectFolderLocate(self) -> None:
         pass
     
-    def projectfoldercancel(self):
+    def projectfoldercancel(self) -> None:
         self.projectlolderlineedit(self)
         
-    
-    def projectlolderlineedit(self):
+    def projectlolderlineedit(self) -> None:
         settings = QSettings()
         
         vProjectLocation = settings.value("Project/Folder", False)
         
         if vProjectLocation != False:
-           self.projectlolderlineedit.setText(vProjectLocation)
-           self.pushButtonProjectFolderSave.setEnabled(True)
+            self.projectlolderlineedit.setText(vProjectLocation)
+            self.pushButtonProjectFolderSave.setEnabled(True)
         else:
-           self.projectlolderlineedit.setText("          Please Enter Project Main Folder")
-           self.pushButtonProjectFolderSave.setEnabled(False)    
-                       
-        
-        
+            self.projectlolderlineedit.setText("Please Enter Project Folder")
+            self.pushButtonProjectFolderSave.setEnabled(False)
+            
